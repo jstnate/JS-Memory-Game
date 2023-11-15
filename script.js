0357f42022034b2701ee0deb568ad211471dd9d7
Charles <charles.chrismann@free.fr>
2023-11-15 21:49:36 +0100

    ADD ui

--- script.js
+++ script.js
@@ -3,87 +3,98 @@ function shuffleArray(inputArray) {
     return inputArray
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-    const cardData = [
-        { id: 1, content: "Good Bye", imageUrl: "<img src='assets/bye.svg'>" },
-        { id: 2, content: "Hello", imageUrl: "<img src='assets/hello.svg'>" },
-        { id: 3, content: "I Love You", imageUrl: "<img src='assets/loveYou.svg'>" },
-        { id: 4, content: "No", imageUrl: "<img src='assets/no.svg'>" },
-        { id: 5, content: "Please", imageUrl: "<img src='assets/please.svg'>" },
-        { id: 6, content: "Sorry", imageUrl: "<img src='assets/sorry.svg'>" },
-        { id: 7, content: "Thank you", imageUrl: "<img src='assets/thankYou.svg'>" },
-        { id: 8, content: "Yes", imageUrl: "<img src='assets/yes.svg'>" },
-    ];
+const cardData = [
+    { id: 1, content: "Good Bye", imageUrl: "<img src='assets/bye.svg'>" },
+    { id: 2, content: "Hello", imageUrl: "<img src='assets/hello.svg'>" },
+    { id: 3, content: "I Love You", imageUrl: "<img src='assets/loveYou.svg'>" },
+    { id: 4, content: "No", imageUrl: "<img src='assets/no.svg'>" },
+    { id: 5, content: "Please", imageUrl: "<img src='assets/please.svg'>" },
+    { id: 6, content: "Sorry", imageUrl: "<img src='assets/sorry.svg'>" },
+    { id: 7, content: "Thank you", imageUrl: "<img src='assets/thankYou.svg'>" },
+    { id: 8, content: "Yes", imageUrl: "<img src='assets/yes.svg'>" },
+];
 
 
-    const gameBoard = document.getElementById('gameBoard');
-    shuffleArray(cardData.map((e, i) => {
-        return [{ content: e.content, id: e.id }, { id: e.id, content: e.imageUrl }];
+const gameBoard = document.getElementById('gameBoard');
+shuffleArray(cardData.map((e, i) => {
+    return [{ content: e.content, id: e.id }, { id: e.id, content: e.imageUrl }];
+})
+    .flat())
+    .forEach((e, i) => {
+        const card = document.createElement('div');
+        card.classList.add('card', 'flipped');
+        card.dataset.cardId = e.id;
+        card.innerHTML = `
+        <div class="card-inner">
+            <div class="front-face">${e.content}</div>
+            <div class="back-face"><img src="assets/3026570.svg" style="width: 80%; height: 80%; object-fit: contain;"/></div>
+        </div>`;
+        gameBoard.appendChild(card);
     })
-        .flat())
-        .forEach((e, i) => {
-            const card = document.createElement('div');
-            card.classList.add('card', 'flipped');
-            card.dataset.cardId = e.id;
-            card.innerHTML = `
-            <div class="card-inner">
-                <div class="front-face">${e.content}</div>
-                <div class="back-face"><img src="assets/3026570.svg" style="width: 80%; height: 80%; object-fit: contain;"/></div>
-            </div>`;
-            gameBoard.appendChild(card);
-        })
 
-    const cards = document.querySelectorAll('.card');
-    let hasFlippedCard = false;
-    let firstCard, secondCard;
-    let lockBoard = false;
+const cards = document.querySelectorAll('.card');
+let hasFlippedCard = false;
+let firstCard, secondCard;
+let lockBoard = false;
 
-    function flipCard() {
-        if (lockBoard) return;
-        if (this === firstCard) return;
+function flipCard() {
+    if (lockBoard) return;
+    if (this === firstCard) return;
 
-        this.classList.remove('flipped');
+    this.classList.remove('flipped');
 
-        if (!hasFlippedCard) {
-            // Premier clic
-            hasFlippedCard = true;
-            firstCard = this;
-            return;
-        }
-
-        // Deuxième clic
-        secondCard = this;
-        checkForMatch();
+    if (!hasFlippedCard) {
+        // Premier clic
+        hasFlippedCard = true;
+        firstCard = this;
+        return;
     }
 
-    function checkForMatch() {
-        let isMatch = firstCard.dataset.cardId === secondCard.dataset.cardId;
+    // Deuxième clic
+    secondCard = this;
+    checkForMatch();
+}
 
-        isMatch ? disableCards() : unflipCards();
-    }
+function checkForMatch() {
+    let isMatch = firstCard.dataset.cardId === secondCard.dataset.cardId;
 
-    function disableCards() {
-        firstCard.removeEventListener('click', flipCard);
-        secondCard.removeEventListener('click', flipCard);
-        firstCard.classList.add('matched');
-        secondCard.classList.add('matched');
+    isMatch ? disableCards() : unflipCards();
+}
+
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+    firstCard.classList.add('matched');
+    secondCard.classList.add('matched');
+    resetBoard();
+}
+
+function unflipCards() {
+    lockBoard = true;
+
+    setTimeout(() => {
+        firstCard.classList.add('flipped');
+        secondCard.classList.add('flipped');
         resetBoard();
-    }
+    }, 1000);
+}
 
-    function unflipCards() {
-        lockBoard = true;
+function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
 
-        setTimeout(() => {
-            firstCard.classList.add('flipped');
-            secondCard.classList.add('flipped');
-            resetBoard();
-        }, 1000);
-    }
+cards.forEach(card => card.addEventListener('click', flipCard));
 
-    function resetBoard() {
-        [hasFlippedCard, lockBoard] = [false, false];
-        [firstCard, secondCard] = [null, null];
-    }
+// const drawer = document.querySelector('main .drawer')
+// let isOpen = false
+// document.querySelector('main .drawer .truc').addEventListener('click', (e) => {
+//     if(drawer.classList.contains('paused')) drawer.classList.remove('paused')
+//     isOpen = !isOpen
+//     // isOpen ? 
 
-    cards.forEach(card => card.addEventListener('click', flipCard));
-});    
+//     const el = drawer
+//     el.style.animation = 'none';
+//     el.offsetHeight; /* trigger reflow */
+//     el.style.animation = null; 
+// })
